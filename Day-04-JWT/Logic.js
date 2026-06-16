97ca7cf7af50667953f556def06692f6143e2941
Vishal <hackone710@gmail.com>
2026-06-17 02:48:02 +0530

    Logic behind Bug solving is added

--- Day-04-JWT/Logic.js
+++ Day-04-JWT/Logic.js
@@ -1,8 +1,8 @@
 /*   
 
-I have written a logic ; 
+                               I have written a logic ; 
 
-Let assume their are 3 endpoints "/signup" , "/signin" , "/me" 
+Their are 3 endpoints "/signup" , "/signin" , "/me" 
 
 While making an auth middleware a problem was occuring that when user signed up and after completing signUp process he should "signIn" first to access "/me" endpoint . But when user was doing so then error was coming " jwt must me assigned " , i dont want that error in fact in place of that error i want that it should print that please sign in first to access 
 "/me" endpoint . 
@@ -15,29 +15,42 @@ So for that i thought that I can do  1 thing and i.e :-
 */  
   
   // -------------  HOW TO SEE ILLUSTRATION -----------------------------------------------
-  // REMOVE issuedToken value thus it will show the illustration of a user having no token., means he is tryig to access "/me" before hitting "/signin" or before loggingIn
+
+// Enter incorrect username or password :- thorws and error simple 
+
+  // REMOVE issuedToken value thus it will show the illustration of a user having no token., means he is tryig to access "/me" before hitting "/signin" or before loggingIn 
 
   // Dont remove issuedToken:-  SHow the illustration that a user is having a token means he was signedIn and now he wants to access "/me" endpoint
   
-  // Enter incorrect username or password :- thorws and error simple 
   
-  const users=[{username:"vishal",password:"123",issuedToken:"hgfhjkbdf"}]  ;
+
+
+  
+  const users=[{username:"vishal",password:"123",issuedToken:"hgfhjkbdf"}];   // Making a dummy users array for illustration
 
     
-    
+    // now we are taking username and password as input from user (backend logic will be used )
     const username="vishal";
     const password="123";
     
     
-    
-    for(let i=0;i<users.length;i++)
+ /* Now we are checking whether the user with given details exists in users array (Database ).
+
+     If yes it means that user was "signedUp previously, but yet it's not confirm that he is " signedIn" or not" 
+   
+        HOW TO FIND:- IF user is having a token then it means that he was "signedIn" if not then  it means he was not signedIn previously.
+   
+ */  
+
+    for(let i=0;i<users.length;i++)  //checking existence of user in dB
         {
 
-          if(users[i].username==username && users[i].password==password)
+   if(users[i].username==username && users[i].password==password) // Checking that does user have token ?
+                                                     
 
             {
 
-const DoesHaveTokenkey="issuedToken" in users[i];
+const DoesHaveTokenkey="issuedToken" in users[i]; // return type = TRUE or FALSE
     console.log(DoesHaveTokenkey);
                         
       if(DoesHaveTokenkey===true)
@@ -58,7 +71,7 @@ console.log("please signIn first to access '/me' endpoint  ");
 
 
             else{
-                console.log("invlid username or password");
+                console.log("invlid username or password,OR user not exists");
                 
             }
        
